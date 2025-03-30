1de237cbefd570b29534ad2e8c5a1a43fb8a6e0e
Boris Lahos <borkolahos@gmail.com>
2025-03-30 18:29:56 +0200

    Malo vise modifikovan sajt dodao sam da dole i gore stoje, fali fond dole da se namesti Irish Grover nesto nece i gore fali onako da rotiram da bude zaokruzeno ako cemo bas tako da radimo mada kontam da je ovako kockasto bolje ti vidi kako ces samo. ZNAM da smo trebali defalut sajt ali taj login deo moze se lagano izbaciti pa da ostanu samo ovaj gornji i donji deo :D

---
+++ Frontend/login/login.js
@@ -0,0 +1,17 @@
+document.getElementById('togglePassword').addEventListener('click', function() {
+    const passwordInput = document.getElementById('password');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+});
+
+function login() {
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (email === '' || password === '') {
+        alert('Please fill in all fields.');
+    } else {
+        // Ovde će kasnije ići prava provera
+        alert('Login Successful (fake)');
+    }
+}
