4e64800f511429a6a6e0c1795efaace46c2a6421
Anja Stamenović <stamenovicanja25@gmail.com>
2025-05-12 17:32:54 +0200

    logika dugmeta Novo

---
+++ Frontend/list_of_products/list1.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.side-category .side-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+      btn.parentElement.classList.toggle('open');
+  });
+});
+
+document.querySelectorAll('.sub-btn').forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    btn.parentElement.classList.toggle('open');
+  });
+});
+
+
