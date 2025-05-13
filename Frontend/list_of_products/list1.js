1413fb61ca1eb4c0cfd0c1fa674694e86d726376
Anja Stamenović <stamenovicanja25@gmail.com>
2025-05-13 14:27:47 +0200

    Gotovo funcionalno poveyivanje 'Novo' i 'Akcija' sa List1

--- Frontend/list_of_products/list1.js
+++ Frontend/list_of_products/list1.js
@@ -1,6 +1,7 @@
+// Sidebar dugmići
 document.querySelectorAll('.side-category .side-btn').forEach(btn => {
   btn.addEventListener('click', () => {
-      btn.parentElement.classList.toggle('open');
+    btn.parentElement.classList.toggle('open');
   });
 });
 
@@ -11,4 +12,24 @@ document.querySelectorAll('.sub-btn').forEach(btn => {
   });
 });
 
+// Filter proizvoda prema URL parametru
+window.addEventListener('DOMContentLoaded', () => {
+  const params = new URLSearchParams(window.location.search);
+  const filter = params.get('filter');
+  const titleElement = document.querySelector('.product-section h2');
+  const productCards = document.querySelectorAll('.product-card');
 
+  if (filter === 'novo') {
+    if (titleElement) titleElement.textContent = 'NOVI PROIZVODI';
+    productCards.forEach(card => {
+      card.style.display = card.classList.contains('novo') ? 'block' : 'none';
+    });
+  } else if (filter === 'akcija') {
+   if (titleElement) titleElement.textContent = 'PROIZVODI NA AKCIJI';
+
+    // Prikazujemo samo proizvode koji imaju klasu 'akcija'
+    productCards.forEach(card => {
+      card.style.display = card.classList.contains('akcija') ? 'block' : 'none';
+    });
+  }
+});
